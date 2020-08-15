cbab517edcddaf31029a9a27ef01492f0ccfb827
Sharath Bangalore Ramesh Kumar <55332048+bsharathramesh@users.noreply.github.com>
2020-08-15 19:31:00 -0400

    Sbangal2 (#6)
    * Adding names to README file
    
    * Adding names to README file
    
    * Removing a file CODE-OF-CONDUCT.md
    
    * Added code to setup.py file

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-Nischal Badarinath Kashyap
+# The setup.py file describes the metadata of the project. 
+
+from distutils.core import setup
+
+setup(
+	name='Code',
+	version='1.0',
+	description='CSC 510 Software Engineering Group Homework 1',
+ 	author='Sharath BR',
+	author_email='sbangal2@ncsu.edu',
+	url='https://github.com/NischalKash/SENG20-G19.git/code',
+	packages=['code'],
+	classifiers=[
+		"MIT License",
+		"Programming Languages :: Python",
+		"Topic :: Software Engineering Course",
+	],
+	license="MIT",
+      )
+
